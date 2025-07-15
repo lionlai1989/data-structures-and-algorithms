3248bf6bb1acb7015a228da7886661e4e8770696
lai <lionlai1989@gmail.com>
2025-07-15 15:35:09 +0200

    bipartite

--- C3-Algorithms_on_Graphs/W3-Paths_in_Graphs_1/2_bipartite.cpp
+++ C3-Algorithms_on_Graphs/W3-Paths_in_Graphs_1/2_bipartite.cpp
@@ -1,24 +1,74 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 
-using std::vector;
-using std::queue;
+using namespace std;
 
-int bipartite(vector<vector<int> > &adj) {
-  //write your code here
-  return -1;
+int bipartite(vector<vector<int>> &adjacency_list) {
+    int n_vertices = adjacency_list.size();
+
+    // +1: white
+    // -1: black
+    //  0: uncolored
+    vector<int> colors(n_vertices, 0);
+    vector<bool> visited(n_vertices, false);
+
+    for (int start = 0; start < n_vertices; ++start) {
+        // There could be different connected componenets.
+        if (visited[start]) {
+            continue;
+        }
+
+        queue<int> q;
+
+        colors[start] = 1; // set to white
+        visited[start] = true;
+        q.push(start);
+
+        while (!q.empty()) {
+            int n_ele = q.size();
+
+            for (int i = 0; i < n_ele; ++i) {
+                int node = q.front();
+                q.pop();
+
+                int curr_color = colors[node];
+
+                for (auto nbr : adjacency_list[node]) {
+                    if (!visited[nbr]) { // not visited
+                        colors[nbr] = curr_color * -1;
+                        visited[nbr] = true;
+                        q.push(nbr);
+                    } else { // already visited
+                        if (curr_color == colors[nbr]) {
+                            return 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return 1;
 }
 
+// g++ 2_bipartite.cpp -std=c++17 -Wall && ./a.out < 2_bipartite.txt
 int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y;
-    std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
-  }
-  std::cout << bipartite(adj);
+    int n_vertices, m_edges;
+
+    while (cin >> n_vertices >> m_edges) {
+        vector<vector<int>> adjacency_list(n_vertices);
+        int u, v;
+
+        for (int i = 0; i < m_edges; ++i) {
+            cin >> u >> v;
+            u -= 1;
+            v -= 1;
+            adjacency_list[u].push_back(v);
+            adjacency_list[v].push_back(u);
+        }
+        cout << bipartite(adjacency_list) << endl;
+    }
+
+    return 0;
 }
